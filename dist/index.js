387fcdcef8a455dd4bd6304876ac2bcd6b5438a1
richarn <rnezferreira9@gmail.com>
2021-02-02 17:57:27 -0300

    creado el puerto de arranque del servidor

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log('hola mundo');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var server_1 = __importDefault(require("./classes/server"));
+var server = new server_1.default();
+//escuchar server
+server.start(function () {
+    console.log("Servidor corriendo en puerto " + server.port);
+});
--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log('holi mundo');
+import Server from "./classes/server";
+
+const server = new Server();
+
+
+//escuchar server
+
+server.start(() => {
+  console.log(`Servidor corriendo en puerto ${ server.port }`);
+  
+});
